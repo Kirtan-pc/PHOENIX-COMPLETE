5d6132471b88efd255adfe1296abe720cb3cdd7b
Kirtan Devadiga <kirtandevadiga@gmail.com>
2026-02-09 21:37:15 +0530

    Fix: Re-enable STATICFILES_DIRS and update templates for local dev

--- phoenix_web/phoenix_web/settings.py
+++ phoenix_web/phoenix_web/settings.py
@@ -70,9 +70,10 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'chatbot' / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
